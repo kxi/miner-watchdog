5d46772d8026638cf0b3e92571757768500f815e
Kai Xi <kaixi.work@gmail.com>
2018-03-31 00:23:17 +1100

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # miner-watchdog
+python miner_watchdog RUN-PGN-SLINGING C:/Users/Kai/Desktop/mining/pigeon/nevermore-x16s-win64 300
---
+++ miner_watchdog.py
@@ -0,0 +1,75 @@
+import subprocess
+import sys
+import os
+import time
+from datetime import datetime
+import re
+from threading import Thread, Event
+
+READ_FLAG =[True]
+
+linebuffer=[]
+
+def run_miner(cmd_name, cmd_path):
+    proc = subprocess.Popen(cmd_name, cwd=cmd_path,
+                            stdout = subprocess.PIPE, stderr = subprocess.PIPE, shell=True)
+    output = proc.stdout
+
+    def reader(f, buffer):
+        while READ_FLAG[0] == True:
+            line=f.readline()
+            if line:
+                buffer.append(line)
+            else:
+                # print("Read Over")
+                break
+
+    t = Thread(target=reader,args=(output, linebuffer))
+    t.daemon=True
+    t.start()
+
+def kill_miner():
+    proc = subprocess.Popen("taskkill /im ccminer.exe /f", stdout = subprocess.PIPE, stderr = subprocess.PIPE, shell=True)
+
+def main():
+
+    cmd_name = sys.argv[1]
+    cmd_path = sys.argv[2]
+    timeout = int(sys.argv[3])
+
+    last_check = datetime.now()
+    count = 0
+
+    run_miner(cmd_name, cmd_path)
+
+    while True:
+        count += 1
+        now = datetime.now()
+        time_delta = (now - last_check).total_seconds()
+        print("\nTime Now = {}, Time Last = {}, Time Elapsed = {} Sec".format(now.strftime("%Y-%m-%d %H:%M:%S"),
+                                                                            last_check.strftime("%Y-%m-%d %H:%M:%S"),
+                                                                            int(time_delta)))
+
+        if time_delta > timeout:
+            print("Miner is Not Responsive, Restart ---> ")
+            kill_miner()
+            READ_FLAG[0] = False
+            print("Miner Restarting, Wait ---> ")
+            time.sleep(10)
+            READ_FLAG[0] = True
+            run_miner(cmd_name, cmd_path)
+            last_check = datetime.now()
+            print("Miner Restarting Complete ---> ")
+
+        while linebuffer:
+            status =(linebuffer.pop()).decode(errors='ignore').rstrip('\n')
+            time_stamp = re.search(r'\d{4}-\d{2}-\d{2}\s{1}\d{2}:\d{2}:\d{2}', status)
+            print("[Miner] {}".format(status))
+            if time_stamp:
+                # print(time_stamp.group(0))
+                last_check = datetime.strptime(time_stamp.group(0), '%Y-%m-%d %H:%M:%S')
+
+        time.sleep(10)
+
+
+main()
